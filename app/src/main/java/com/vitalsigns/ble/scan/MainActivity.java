d2d700794694f6f6b5098db7094894c61ebf915e
AllenTeng (Mac-Mini) <allen_teng@vsigntek.com>
2017-07-26 17:52:30 +0800

    Avoid duplicated device

--- app/src/main/java/com/vitalsigns/ble/scan/MainActivity.java
+++ app/src/main/java/com/vitalsigns/ble/scan/MainActivity.java
@@ -4,19 +4,25 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.le.ScanResult;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.vitalsigns.sdk.ble.scan.BleScanner;
 import com.vitalsigns.sdk.ble.scan.BleScannerInterface;
 import com.vitalsigns.sdk.utility.RequestPermission;
+import com.vitalsigns.sdk.utility.Utility;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
+  private static final String LOG_TAG = "MainActivity";
   private static final int BLE_SCAN_TIIMEOUT = 10 * 1000;
 
   private BleScanner mBleScanner = null;
   private boolean mScanStart = false;
+  private ArrayList<String> mDeviceMacList = null;
 
   @Override
   protected void onCreate(Bundle savedInstanceState)
@@ -30,6 +36,10 @@ public class MainActivity extends AppCompatActivity
   {
     super.onStart();
 
+    /// [AT-PM] : Initialize list ; 07/26/2017
+    mDeviceMacList = new ArrayList<>();
+
+    /// [AT-PM] : Start BLE scanner ; 07/26/2017
     mBleScanner = new BleScanner();
     if(mBleScanner.initialize(this, mBleScannerFailInitialize))
     {
@@ -46,11 +56,16 @@ public class MainActivity extends AppCompatActivity
   {
     super.onStop();
 
+    /// [AT-PM] : Stop BLE scanner ; 07/26/2017
     if(mScanStart)
     {
       mBleScanner.stopScan();
       mScanStart = false;
     }
+
+    /// [AT-PM] : Release memory ; 07/26/2017
+    mDeviceMacList.clear();
+    mDeviceMacList = null;
   }
 
   private BleScannerInterface.OnDeviceFound mBleScannerDeviceFound = new BleScannerInterface.OnDeviceFound()
@@ -63,7 +78,13 @@ public class MainActivity extends AppCompatActivity
       String name = bluetoothDevice.getName();
       String mac = bluetoothDevice.getAddress();
 
-      ((LinearLayout)findViewById(R.id.list)).addView(new ListItemView(MainActivity.this).setInfo(name, rssi, mac));
+      /// [AT-PM] : Add into list ; 07/26/2017
+      if(!mDeviceMacList.contains(name))
+      {
+        mDeviceMacList.add(name);
+        ((LinearLayout)findViewById(R.id.list)).addView(new ListItemView(MainActivity.this).setInfo(name, rssi, mac));
+        Log.d(LOG_TAG, String.format("Find %d device %s(%s) with RSSI = %d", mDeviceMacList.size(), name, mac, rssi));
+      }
     }
   };
 
